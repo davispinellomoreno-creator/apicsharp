5bdb4f745473833ee49d4d4301d90ada15559ee7
davispinellomoreno-creator <davispinellomoreno@gmail.com>
2026-09-12 23:39:45 -0300

    !Criação das pastas restantes

--- obj/Debug/net10.0/meuprojeto.AssemblyInfo.cs
+++ obj/Debug/net10.0/meuprojeto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("meuprojeto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2f712a06cab6382d01b101a176e42a7701588fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55d78c8e2473fded72262514edab496bb3d8522f")]
 [assembly: System.Reflection.AssemblyProductAttribute("meuprojeto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("meuprojeto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
